fe30036d0bae69d59d9f2af8a88a3e0b316f6d30
KeoCode <keocode101@gmail.com>
2022-10-23 17:17:59 +0000

    Added replay function

--- run.py
+++ run.py
@@ -31,20 +31,19 @@ def intro_to_game():
     """
     username = input("Welcome! Please enter your Name:\n")
     print(f"Hi {username}, You have upto 6 guesses to guess the Secret Word.")
-    input("when you are ready to play, Press any key to start")
+    input("when you are ready to play, Press the Enter key to start")
 
 
 def get_word():
     """
     Pick a random word from the list we saved into the data variable
     """
-    word = random.choice(data)
-    return word
+    return random.choice(data)
 
 
 def play_game():
     """
-    Main Function for the game play. it will print out the length of letters
+    Main Function for the game play. it will print out the length of is
     in the word and check if guesses are correct and reveal if they are.
     will also check how many lives you have and print message if you
     win or lose
@@ -54,24 +53,28 @@ def play_game():
     guesses = []
     complete = False
 
-    while not complete:
-        for letter in word:
-            if letter.lower() in guesses:
-                print(letter, end=" ")
+    print('The word contains', len(word), 'letters.')
+    print(word)
+
+    while complete is False:
+        for i in word:
+            if i.lower() in guesses:
+                print(i, end="")
             else:
-                print("_", end=" ")
+                print("_", end="")
         print("")
 
-        guess = input(f"Lives left:{lives}, Guess again:")
+        guess = input(f"Lives left:{lives}, Guess: ")
         guesses.append(guess.lower())
         if guess.lower() not in word:
             lives -= 1
+            print(f" Guessed already: {guesses}")
             if lives == 0:
                 break
 
     complete = True
-    for letter in word:
-        if letter.lower() not in guesses:
+    for i in word:
+        if i.lower() not in guesses:
             complete = False
 
     if complete:
@@ -80,12 +83,25 @@ def play_game():
         print(f"Oh no! Game over! The Secret Word was {word}")
 
 
+def replay():
+    """
+    once game is finished, ask player if they would like to play again,
+    if so restart play game function, if not then print message
+    """
+    replay_answer = input("would you like to play again? Enter Y/N").lower()
+    if replay_answer == 'y':
+        play_game()
+    else:
+        print("Thanks for playing! Hope to see you again soon!")
+
+
 def main():
     """
     All the main function of game play in order
     """
     intro_to_game()
     play_game()
+    replay()
 
 
 main()
